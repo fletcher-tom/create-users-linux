c388dfa0d50743cf13568687dfcabe8174311049
tom <tom-fletcher@outlook.com>
2020-10-14 15:11:55 +0100

    Script updated to meet Requirement 2.i

--- create_user.sh
+++ create_user.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+USERNAME="$1"
+PASSWORD="$2"
 
 # Add user account with name passed in as first parameter
 sudo useradd $USERNAME
 
-# Set a password (which is passed in as second parameter) against the user created, and ONLY send successful message if chpasswd exits successfully
+# Check if password was passed in 
+# -z = check if string is null, i.e. check if $PASSWORD has zero length
+if [ -z $PASSWORD];
+# If no password passed in, then randomly generate one and assign it to the PASSWORD variable.
+then 
+    PASSWORD=$(openssl rand -base64 32)
+fi
+
+# Set a password against the user created, and ONLY send successful message if chpasswd exits successfully
 # && = only run command on right if the command on the left has run successfully
-echo "$USERNAME:$PASSWORD" | sudo chpasswd && echo "Account for $USERNAME has been created"
+ echo "$USERNAME:$PASSWORD" | sudo chpasswd && echo "Account for $USERNAME has been created"
 
